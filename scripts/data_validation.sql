d0f4f54da2f5ecab2404987053aa02fac7a97570
andylim-lw <andy.lim@lifewood.com>
2024-02-24 10:29:33 +0800

    Add labels

--- scripts/data_validation.sql
+++ scripts/data_validation.sql
@@ -1,6 +1,9 @@
+--Sales: Aggregate sum of order items' total.
 select ROUND(SUM(total)::NUMERIC, 2)::double precision from feedme.order_items;
 
+--Quantity: Aggregate count of orders.
 select count(*) from feedme.orders;
 
+--Item Quantity: Aggregate sum of order items' quantity.
 select sum(quantity)
 from feedme.order_Items;
